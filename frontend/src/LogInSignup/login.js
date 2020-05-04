08f217fb87a74d8d2e585bf37ec4f8cc40462e9e
Ziang Hong <45635887+ZaHong@users.noreply.github.com>
2020-05-03 21:05:31 -0700

    Updated login

--- frontend/src/LogInSignup/login.js
+++ frontend/src/LogInSignup/login.js
@@ -1,44 +1,131 @@
 import React, { Component } from 'react';
-import logo from "./res/MyFridge_Logo.png";
-import "./login.css"
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import logo from './res/MyFridge_Logo.png';
 
-const style = {
-    margin: 15,
-};
-
-class Login extends Component {
+class Login extends Component{
   constructor(props){
     super(props);
+    
     this.state = {
-        username: '',
-        password: ''};
+      username: '',
+      password: ''
+    }
   }
 
-  handleLogin(event){
-    event.preventDefault();
-    const { email, password } = event.target.elements;
+  handleClick(event){
+
   }
 
   render(){
-    return (
-      <div className="login">
-          <div class="title"><h1>Welcome to MyFridge</h1></div>
-          <div class="imgcontainer">
-              <img src={logo} alt="MyFridge Logo"></img>
-          </div>
+    const { classes } = this.props;
+    const root = {
+      height: '100vh',
+    };
 
-          <div class="container">
-            <form onSubmit={this.handleLogin}>
-                <label>Email: </label>
-                <input name="email" type="email" placeholder="Enter Email..."/>
-                <label>Password: </label>
-                <input name='password' type="password" placeholder="Enter password..." />
-                <button type="submit">Log In</button>
+    const image = {
+      backgroundImage: 'url('+ logo +')',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+    };
+
+    const paper = {
+      marginTop: 8,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    };
+
+    const avatar = {
+      marginTop: '5vh',
+    };
+
+    const form = {
+      width: '100%',
+      marginTop: '5vh',
+    };
+
+    const submit = {
+      margin: 'auto',
+      spacing: [3],
+    };
+
+    return (
+      <Grid container component="main" style={root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} style={image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div style={paper}>
+            <Avatar style={avatar}><LockOutlinedIcon /></Avatar>
+            <Typography component="h1" variant="h5">Sign in</Typography>
+            <form style={form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange = {(event, newValue) => this.setState({username:newValue})}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange = {(event,newValue) => this.setState({password:newValue})}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                style={submit}
+                onClick={(event) => this.handleClick(event)}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item >
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
             </form>
           </div>
-      </div>
+        </Grid>
+      </Grid>
     );
   }
 }
+
+
 
 export default Login;
