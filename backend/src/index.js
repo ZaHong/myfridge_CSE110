bc777804a835d23701ae348bbc8053d74a069d09
Richard <gsu@ucsd.edu>
2020-05-08 19:16:58 -0700

    connect register router with db

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const apiPort = 8000
+const user_router = require('./route/user_action')
 
 // const {client, addUser} = require('./mongo.js')
 
@@ -14,5 +15,7 @@ app.get('/', async (req, res) => {
     // addUser("nope", "hak")
     res.send('Hello From the Backend!')
 })
+
+app.use('/user', user_router)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
