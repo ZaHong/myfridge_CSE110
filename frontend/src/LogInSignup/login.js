21dcb286f3fdeb8ae4e9c195a3c8352c5cc9d9d1
Ziang Hong <45635887+ZaHong@users.noreply.github.com>
2020-05-12 01:26:08 -0700

    login

--- frontend/src/LogInSignup/login.js
+++ frontend/src/LogInSignup/login.js
@@ -3,23 +3,83 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import logo from './res/MyFridge_Logo.png';
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Background_Image from './res/background_img.png';
+import logo from './res/MyFridge_Logo_Small.png';
+import background_color from './res/background_color.png';
 
-class Login extends Component{
-  constructor(props){
+const styles = theme => ({
+  background:{
+    backgroundImage: 'url('+ background_color +')',
+    backgroundSize: 'cover',
+    padding: '0 6px',
+    height: '100%',
+    position: 'fixed',
+    overflow: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  image: {
+    backgroundImage: 'url('+ logo +')',
+    //backgroundRepeat: 'no-repeat',
+    //backgroundPosition: 'center',
+    //backgroundSize: 'cover',
+    //padding: '0 6px',
+    //height: '100%',
+    //position: 'fixed',
+    //overflow: 'auto',
+  },
+  paper: {
+    marginTop: theme.spacing(1),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  outerpaper:{
+    marginBottom: 'auto',
+    border: 'solid 25px white',
+    borderRadius: '20px',
+    padding: '5 2vw',
+    minHeight: '80vh',
+    boxSizing: 'border-box',
+    //boxShadow: '0px 20px 4px rgba(0, 0, 0, 0.25)',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: '#5f5f5d',
+  },
+  form: {
+    width: '90%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  link: {
+    color:"#5f5f5d"
+  },
+  notchedOutline: {
+    borderWidth: "2px",
+    borderColor: "white !important"
+  }
+});
+
+
+class Login extends Component {
+  constructor(props) {
     super(props);
-    
+    document.title = "Sign In"
+
     this.state = {
-      email: '',
-      password: ''
-    }
+      email:"",
+      password:""
+    };
   }
 
   handleClick(event){
@@ -28,7 +88,7 @@ class Login extends Component{
       email : this.state.email,
       password : this.state.password,
     };
-    alert(JSON.stringify(user))
+    //alert(JSON.stringify(user))
     
     //For create new user
     fetch("http://localhost:8000/user/register",{
@@ -44,105 +104,87 @@ class Login extends Component{
     this.setState({[name]: event.target.value})
   }
 
-  render(){
-    const root = {
-      height: '100vh',
-    };
+  render() {
 
-    const image = {
-      backgroundImage: 'url('+ logo +')',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-    };
-
-    const paper = {
-      marginTop: 8,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    };
-
-    const avatar = {
-      marginTop: '5vh',
-    };
-
-    const form = {
-      width: '100%',
-      marginTop: '5vh',
-    };
-
-    const submit = {
-      margin: 'auto',
-      spacing: [3],
-    };
-
+    const { classes } = this.props;
     return (
-      <Grid container component="main" style={root}>
+      <Grid container xs={12} className={classes.background}
+      >
+      <img src={logo} marginTop='20px'/>
+      <Grid container xs={10} sm={6} md={4} className={classes.outerpaper}>
+      <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Grid item xs={false} sm={4} md={7} style={image} />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <div style={paper}>
-            <Avatar style={avatar}><LockOutlinedIcon /></Avatar>
-            <Typography component="h1" variant="h5">Sign in</Typography>
-            <form style={form} noValidate>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoFocus
-                defaultValue={this.state.email}
-                onChange={this.handleChange('email')}
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                defaultValue={this.state.password}
-                onChange={this.handleChange('password')}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                style={submit}
-                onClick={event => this.handleClick(event)}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item >
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </form>
-          </div>
-        </Grid>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoFocus
+              InputProps={{
+                classes: {
+                  notchedOutline: classes.notchedOutline
+                }
+              }}
+              defaultValue={this.state.email}
+              onChange={this.handleChange('email')}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              InputProps={{
+                classes: {
+                  notchedOutline: classes.notchedOutline
+                }
+              }}
+              defaultValue={this.state.password}
+              onChange={this.handleChange('password')}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="#5f5f5d"
+              className={classes.submit}
+              onClick={event => this.handleClick(event)}
+            >
+              Sign In
+            </Button>
+            <Grid container 
+              direction="column"
+              alignItems="center"
+              justify="center"
+            >
+            <Link href="#" variant="body2" className={classes.link}>
+                  Forgot password?
+            </Link>
+            <Link href="#" variant="body2" className={classes.link}>
+                  {"Don't have an account? Sign Up"}
+            </Link>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+      </Grid>
       </Grid>
     );
   }
 }
 
-
-
-export default Login;
+export default withStyles(styles)(Login)
